324381a08423e75e4b22642dfaab5fffdbb9264f
Unknown <davidjejans@hotmail.nl>
2018-07-02 20:30:58 +0200

    Conveyor belt functionality
    Need to check why the box doesnt stay on the collision platforms.

---
+++ Assets/Game/Content/Scripts/David/ConveyorBelt/MovingPlatforms.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour {
+    public float m_Speed;
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x + (m_Speed * Time.deltaTime), transform.position.y, transform.position.z);
+	}
+}
