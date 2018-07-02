ea7a55a4d2ba0c98c6dc2deff30e753a9ba02f58
Unknown <davidjejans@hotmail.nl>
2018-07-02 13:28:26 +0200

    object hover and click functionality

---
+++ Assets/Game/Content/Scripts/David/GlowOnHover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowOnHover : MonoBehaviour {
+    [SerializeField] private Material m_HoverMaterial;
+    [SerializeField] private Material m_ClickMaterial;
+    private Material m_OriginalMaterial;
+    private MeshRenderer m_ObjectMesh;
+
+    private void Start()
+    {
+        m_ObjectMesh = this.GetComponent<MeshRenderer>();
+        m_OriginalMaterial = m_ObjectMesh.material;
+    }
+
+    private void OnMouseEnter()
+    {
+        m_ObjectMesh.material = m_HoverMaterial;
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            m_ObjectMesh.material = m_ClickMaterial;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        m_ObjectMesh.material = m_OriginalMaterial;
+    }
+}
