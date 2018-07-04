9377566c65657ff5a0beaeb8f9906c22ecacd4ea
Glenn.L <526228@student.gl.nl>
2018-07-04 13:55:55 +0200

    Auto stash before merge of "master" and "origin/master"
    Trying to fix the material change

---
+++ Assets/Game/Content/Scripts/Glenn/BoxScanner.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxScanner : MonoBehaviour
+{
+     public Material m_ScannerMaterial;
+    void OnTriggerEnter(Collider col)
+    {
+        Destroy(col.gameObject);
+    }
+}
