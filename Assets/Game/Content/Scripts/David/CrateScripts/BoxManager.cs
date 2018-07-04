c4aaa499125a8aaee132ac4876e675519f91e00b
Unknown <davidjejans@hotmail.nl>
2018-07-04 09:54:07 +0200

    Time to pull bois !

--- Assets/Game/Content/Scripts/David/CrateScripts/BoxManager.cs
+++ Assets/Game/Content/Scripts/David/CrateScripts/BoxManager.cs
@@ -3,5 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxManager : MonoBehaviour {
+    [SerializeField] private string m_CurrentCountry;
     [SerializeField] private List<GameObject> m_ItemsInCrate;
+
+    private void CheckItems()
+    {
+        for (int i = 0; i < m_ItemsInCrate.Count; i++)
+        {
+            CheckIfIllegal(m_ItemsInCrate[i]);
+        }
+    }
+
+    private void CheckIfIllegal(GameObject CurrentItem)
+    {
+        if(m_CurrentCountry == "Netherlands")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
 }
