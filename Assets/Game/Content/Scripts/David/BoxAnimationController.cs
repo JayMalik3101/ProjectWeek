0528bb5a62e5c6d0eae5b0cb4983b88bc76b2c2c
Unknown <davidjejans@hotmail.nl>
2018-07-04 08:38:05 +0200

    pre start push

---
+++ Assets/Game/Content/Scripts/David/BoxAnimationController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxAnimationController : MonoBehaviour {
+    private Animator m_BoxAnimator;
+    public bool m_BoxState = true;
+
+    public bool BoxState
+    {
+        set
+        {
+            m_BoxState = value;
+            if (value == true)
+                m_BoxAnimator.SetTrigger("Activate");
+            else
+                m_BoxAnimator.SetTrigger("Deactivate");
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Game/Content/Scripts/David/ConveyorBelt/MovingPlatforms.cs
+++ Assets/Game/Content/Scripts/David/ConveyorBelt/MovingPlatforms.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class MovingPlatforms : MonoBehaviour {
     public float m_Speed;
-	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(transform.position.x + (m_Speed * Time.deltaTime), transform.position.y, transform.position.z);
+    private Rigidbody m_PlatformRB;
+
+    private void Start()
+    {
+        m_PlatformRB = GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update () {
+        m_PlatformRB.MovePosition(new Vector3(transform.position.x + (m_Speed * Time.deltaTime), transform.position.y, transform.position.z));
 	}
 }
