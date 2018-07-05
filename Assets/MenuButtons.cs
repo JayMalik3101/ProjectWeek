55607a6fa188ee023cdb3c5583f120f5ad3e6ca6
Unknown <davidjejans@hotmail.nl>
2018-07-05 21:47:58 +0200

    MainMenu Works

---
+++ Assets/MenuButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour {
+    [SerializeField] private GameObject m_GameManager;
+    [SerializeField] private GameObject m_MenuButtons;
+    [SerializeField] private GameObject m_Info;
+	public void StartGame()
+    {
+        m_Info.SetActive(true);
+        m_GameManager.SetActive(true);
+        m_MenuButtons.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
